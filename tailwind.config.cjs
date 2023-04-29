251becd4d14ab2bfc0a3356ea687af0a2d32548c
Leonardo <leon.peralta123456774@gmail.com>
2023-04-28 21:11:27 -0300

    Navbar final

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	darkMode: 'class',
 	content: [
 		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
 		'./node_modules/flowbite/**/*.js'
 	],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'blanco': '#f3f8f2',
+				'negro': '#191919'
+			}
+		},
 	},
 	plugins: [
 		require('flowbite/plugin')
