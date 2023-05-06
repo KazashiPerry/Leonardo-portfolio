4285f6396844d55c9c1bc9b939d83fc5ebeae4f4
Leonardo <leon.peralta123456774@gmail.com>
2023-05-06 02:44:19 -0300

    Creando Services

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,10 +11,15 @@ module.exports = {
 				'blanco': '#f2f2f2',
 				'negro': '#121212',
 				'gris': '#262626',
-				'celeste': '#79c0ea'
+				'celeste': '#79c0ea',
+				'crema': '#f6eec0',
+				'celeste-crema': '#bae6fd'
 			},
 			backgroundImage: {
-				'svg-image': "url('/public/wave-haikei.svg')"
+				'svg-image': "url('/public/wave-haikei.svg')",
+				'svg-image-top': "url('/public/wave-haikei-top.svg')",
+				'svg-crema-top': "url('/public/wave-haikei-crema-top.svg')",
+				'svg-crema-bot': "url('/public/wave-haikei-crema-bot.svg')"
 			}
 		},
 	},
